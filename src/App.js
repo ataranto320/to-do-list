6dd5704cec63619660a858cdf0b484710e8d6979
Austin T <austin.taranto@gmail.com>
2019-10-28 15:44:17 -0400

    added h1 and todos this.state

--- src/App.js
+++ src/App.js
@@ -17,7 +17,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-
+        <h1 className="center blue-text">Todos</h1>
+        <Todos todos={this.state.todos}/>
       </div>
     )
   }
