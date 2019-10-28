de5693b797103110804fd33089d68f9019bb98b8
Austin T <austin.taranto@gmail.com>
2019-10-28 15:20:07 -0400

    start of list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    todos: [
+      {id: 1, content: "study more code"},
+      {id: 2, content: "collect more whiskey"},
+      {id: 3, content: "oreos for holiday brownies"},
+      {id: 4, content: "collect more wine"},
+      {id: 5, content: "rewatch all Star Wars movies before Ep. IX"}
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    )
+  }
 }
 
 export default App;
