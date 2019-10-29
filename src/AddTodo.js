5ae35735127f29e3c96aac131c21325c292bbff3
Austin T <austin.taranto@gmail.com>
2019-10-29 18:13:03 -0400

    this.handlesubmit added to form onsubmit render

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -19,7 +19,7 @@ class AddTodo extends Component {
     render(){
         return (
             <div>
-                <form onSubmit={}>
+                <form onSubmit={this.handleSubmit}>
                     <label>Add New Todo:</label>
                     <input type="text" onChange={this.handleChange} />
                 </form>
