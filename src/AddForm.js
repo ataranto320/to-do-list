75be2fb6abb22c9076299dc1f2ad2aad988e8c6b
Austin T <austin.taranto@gmail.com>
2019-10-29 17:54:43 -0400

    added submit form for adding to list

--- src/AddForm.js
+++ src/AddForm.js
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
 
 class AddTodo extends Component {
-    
+    // render jsx template
+    render(){
+        return (
+            <div>
+                <form onSubmit={}></form>
+            </div>
+        )
+    }
 }
